35f85fd2dba70ede324d04f332510fdd1f80c049
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2020-11-10 18:39:52 +0900

    feat: enabled delete list request (#16)

--- client/src/components/ListIndex.tsx
+++ client/src/components/ListIndex.tsx
@@ -1,24 +1,28 @@
 import React, { FC } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
 
 import List from '../types/list'
+import axios from 'axios';
 
 type Props = {
   lists: List[];
 }
 
 const ListIndex: FC<Props> = ({ lists }) => {
+  const router = useRouter();
+
   return (
     <div className="divide-y divide-beige">
       <h1 className="pb-4 text-center">AccompList</h1>
 
       {lists.map(list => {
         return (
-          <div key={list.id} className="grid grid-cols-9">
+          <div key={list.ID} className="grid grid-cols-9">
 
             <div className="col-span-1 pt-4">
-              <Link href="/lists/[list-id]" as="/lists/1">
+              <Link href="/lists/[list-id]" as={`/lists/${list.ID}`}>
                 <a className="align-middle pr-2 text-blue text-3xl hover:text-red">
                   {
                     list.done ?
@@ -41,7 +45,7 @@ const ListIndex: FC<Props> = ({ lists }) => {
             <div className="col-span-2">
               {
                 list.done ?
-                  <Link href="/lists/[list-id]" as={`/lists/${list.id}`}>
+                  <Link href="/lists/[list-id]" as={`/lists/${list.ID}`}>
                     <button className="button w-20 text-center my-4 ml-auto">
                       <a>done!</a>
                     </button>
@@ -54,6 +58,10 @@ const ListIndex: FC<Props> = ({ lists }) => {
                 <FontAwesomeIcon
                   className="text-4xl text-blue hover:text-red cursor-pointer mr-4 mb-2 pt-4"
                   icon="trash"
+                  onClick={() => {
+                    axios.delete(`${process.env.ACCOMPLIST_API_BROWSER}/lists/${String(list.ID)}`)
+                    router.push(`/users/${list.user_id}`);
+                  }}
                 />
               </div>
             </div>
