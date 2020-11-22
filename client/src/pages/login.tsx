ad29a7368adfd995f62b61a13d262bc4260b0645
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2020-11-22 13:20:23 +0900

    fix: modified cookie handling (#20)

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 
 import { Layout } from '../components/layout';
+import { divideCookie, setCookies } from '../utils/mycookie';
 
 export default function Login() {
+  const [flash, setFlash] = useState<boolean>(false)
   const router = useRouter();
   const validation = () =>
     Yup.object().shape({
@@ -23,6 +26,15 @@ export default function Login() {
     <Layout>
       <div className="rounded bg-blue h-100 w-auto md:w-1/2 mx-auto my-20 py-32 px-10 text-beige">
         <h1 className="text-center text-2xl pb-10">Welcome back.</h1>
+        {
+          flash ?
+            <div className="rounded text-red text-center">
+              <p>※ログインに失敗しました。</p>
+              <p>※もう一度確認してください。</p>
+            </div>
+            :
+            ""
+        }
         <Formik
           initialValues={{ email: '', password: '' }}
           validationSchema={validation()}
@@ -32,24 +44,16 @@ export default function Login() {
             params.append("password", values.password)
             axios.post(`${process.env.ACCOMPLIST_API_BROWSER}/users/login`, params)
               .then((res) => {
-                // todo
-                const sessionID = res.data.sessionID;
+                console.log(res);
                 const userID = res.data.userID;
-                document.cookie = `userID=${userID}`;
-                document.cookie = `sessionID=${sessionID}`;
+                const sessionID = res.data.sessionID;
+                setCookies(userID, sessionID)
 
-                let cookies = document.cookie;
-                let cookiesArray = cookies.split(';');
-                for (let c of cookiesArray) {
-                  let cArray = c.split('=');
-                  if (cArray[0] == 'userID') {
-                    router.push(`/users/${cArray}`)
-                  }
-                }
+                const content = divideCookie();
+                router.push(`/users/${content["userID"]}`)
               })
-              .catch(function (error) {
-                // todo
-                console.log(error);
+              .catch(() => {
+                setFlash(true)
               })
           }
           }
@@ -84,10 +88,18 @@ export default function Login() {
               <button type="submit" className="button w-full p-1 mt-10">
                 ログイン
               </button>
+
+              <div className="text-center mt-8 hover:text-red">
+                <Link href="/signup">
+                  <a>
+                    新規登録の方はこちら
+                  </a>
+                </Link>
+              </div>
             </form>
           )}
         </Formik>
       </div>
-    </Layout>
+    </Layout >
   );
 }
