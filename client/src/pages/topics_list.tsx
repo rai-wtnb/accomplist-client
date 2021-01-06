e1ef9c484453a51e409c3da2a2c9270e2977a23c
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2021-01-06 05:07:24 +0000

    feat: created all pages

---
+++ client/src/pages/topics_list.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { NextPage, GetServerSideProps } from 'next';
+import axios from 'axios';
+
+import { Layout } from '../components/layout';
+import Menu from '../components/Menu';
+import Profile from '../components/Profile';
+import ListIndex from '../components/ListIndex';
+import User from '../utils/types/user';
+import List from '../utils/types/list';
+import TodoRegister from '../components/TodoRegister';
+import { getUserCookie } from '../utils/mycookie';
+
+const UserPage: NextPage<Props> = () => {
+  const userID = getUserCookie();
+  const router = useRouter();
+
+  useEffect(() => {
+    !userID && router.push(`/login`)
+  }, [])
+
+  return (
+    <Layout>
+      <div className="md:grid grid-cols-3 gap-2 relative pt-4 pb-12 md:pb-32">
+        <div className="col-span-2 rounded border-beige border-2 p-2 divide-y divide-beige text-center">
+          <h1>Topics</h1>
+          <h1>Coming soon...</h1>
+        </div>
+
+        <Menu userID={userID} />
+      </div>
+    </Layout>
+  )
+}
+
+export default UserPage;
