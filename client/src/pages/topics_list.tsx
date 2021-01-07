f9e276e16eec0e65bb6f465825d52c2adad00274
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2021-01-07 09:42:23 +0900

    fix: modifier menu paths (#45)

--- client/src/pages/topics_list.tsx
+++ client/src/pages/topics_list.tsx
@@ -1,12 +1,22 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
 import { Layout } from '../components/layout';
 import Menu from '../components/Menu';
 import { getUserCookie } from '../utils/mycookie';
+import axios from 'axios';
+import User from '../utils/types/user';
+import Feedback from '../utils/types/feedback';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const TopicsList: NextPage = () => {
+type Props = {
+  users: User[];
+  feedbacks: Feedback[];
+}
+
+const TopicsList: NextPage<Props> = ({ users, feedbacks }) => {
   const userID = getUserCookie();
   const router = useRouter();
 
@@ -16,16 +26,36 @@ const TopicsList: NextPage = () => {
 
   return (
     <Layout>
-    <div className="md:grid grid-cols-3 gap-2 relative pt-4 pb-12 md:pb-32">
-      <div className="col-span-2 rounded border-beige border-2 p-2 divide-y divide-beige text-center">
-        <h1>Topics</h1>
-        <h1>Coming soon...</h1>
-      </div>
+      <div className="md:grid grid-cols-3 gap-2 relative pt-4 pb-12 md:pb-32">
+        <div className="col-span-2 rounded border-beige border-2 p-2 divide-y text-center">
+          <h1>話題のリスト</h1>
 
-      <Menu userID={userID} />
-    </div>
+          {
+            feedbacks.map((feedback) => {
+              return(
+                <h1>{feedback.title}</h1>
+              )
+            })
+          }
+        </div>
+
+        <Menu userID={userID} />
+      </div>
     </Layout>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const userRes = await axios.get(`${process.env.ACCOMPLIST_API}/users`)
+  const users: User[] = await userRes.data;
+  const feedbackRes = await axios.get(`${process.env.ACCOMPLIST_API}/feedbacks`)
+  const feedbacks: Feedback[] = await feedbackRes.data;
+  return {
+    props: {
+      users,
+      feedbacks,
+    },
+  };
+};
 
 export default TopicsList;
